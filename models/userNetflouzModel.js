d12aa2780a79c7caf8d0f50ae244c35d83dbc336
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-06 10:09:08 +0200

    bcrypt_OK

--- models/userNetflouzModel.js
+++ models/userNetflouzModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userNetflouzSchema = new Schema({
     name: {
@@ -19,5 +20,24 @@ const userNetflouzSchema = new Schema({
         required: true
     }
 });
+
+// Pre-save hook to hash the password before saving it to the database
+userNetflouzSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// Method to compare plain text password with hashed password
+userNetflouzSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password)
+       .then(match => match);
+};
 
 module.exports = mongoose.model("UserNetflouz", userNetflouzSchema);
